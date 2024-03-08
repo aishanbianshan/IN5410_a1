f49431e392ecc546660329b1b5ffa5ad68e104d8
fredrikofstad <nokotin@gmail.com>
2024-03-08 13:37:13 +0100

    plotting and random

--- energy.py
+++ energy.py
@@ -1,28 +1,41 @@
-"""
-@param peak_hours - list of peak hours
-@param peak_cost - the cost of energy during peak hours
-@param non_peak_cost - the cost of energy during non-peak hours
-returns a list of energy prices varying from peak and non-peak hours
-"""
-def time_of_use(peak_hours, peak_cost, non_peak_cost):
-    prices = []
-    for hour in range(24):
-        if hour in peak_hours:
-            prices.append(peak_cost)
-        else:
-            prices.append(non_peak_cost)
-    return prices
+import random
+import matplotlib.pyplot as plt
 
-def real_time_pricing():
-    # TODO: same but with randomness
-    pass
+def time_of_use(peak_hours: range, peak_cost: float, non_peak_cost: float):
+    """
+    @param peak_hours - list of peak hours
+    @param peak_cost - the cost of energy during peak hours
+    @param non_peak_cost - the cost of energy during non-peak hours
+    returns a list of energy prices varying from peak and non-peak hours
+    """
+    return [peak_cost if hour in peak_hours else non_peak_cost for hour in range(24)]
 
+
+def real_time_pricing(peak_hours: range, peak_cost: float, non_peak_cost: float, fluctuation: float):
+    return [peak_cost + random.uniform(-fluctuation, fluctuation) if hour in peak_hours
+            else non_peak_cost + random.uniform(-fluctuation, fluctuation)
+            for hour in range(24)]
+
+
+def plot_price_curve(price_curve: list):
+    plt.plot(price_curve)
+    plt.xlabel("Hour")
+    plt.ylabel("NOK per kWh")
+    plt.title("Price curve")
+    plt.ylim(0)
+    plt.plot(price_curve, marker="o", linestyle="-")
+    plt.show()
 
 
 if __name__ == "__main__":
     peak_hours = range(17, 20)
     peak_cost = 1
     non_peak_cost = 0.5
-    hourly_cost = time_of_use(peak_hours, peak_cost, non_peak_cost)
-    print(hourly_cost)
+    fluctuations = 0.125
+    hourly_cost_tou = time_of_use(peak_hours, peak_cost, non_peak_cost)
+    print(hourly_cost_tou)
+    hourly_cost_rtp = real_time_pricing(peak_hours, peak_cost, non_peak_cost, fluctuations)
+    print(hourly_cost_rtp)
+    plot_price_curve(hourly_cost_rtp)
+
 
